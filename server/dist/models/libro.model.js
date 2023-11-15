ff75df2d806c4b2a5d9c4265c1de49a565740781
FeijooCiro <128099617+FeijooCiro@users.noreply.github.com>
2023-11-15 19:32:00 -0300

    Delete server directory

--- server/dist/models/libro.model.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=libro.model.js.map
--- server/dist/routes/libros.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => {
-    res.render('libros');
-});
-exports.default = router;
-//# sourceMappingURL=libros.routes.js.map
